37b246f9d32b87a45593c6100e6b6278f9badb44
Borislav Borislavov <borislavkb@gmail.com>
2021-07-23 14:40:18 +0200

    Add CharacterCard.js

---
+++ src/components/CharacterCard.js
@@ -0,0 +1,10 @@
+import "./CharacterCard.css";
+
+export default function CharacterCard() {
+  return (
+    <li className="character-card">
+      <img src="#" alt="character" />
+      <h3>Name</h3>
+    </li>
+  );
+}
