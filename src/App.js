e48ee53cfc0bcaef21dac7baab1c19059cc62898
Borislav Borislavov <borislavkb@gmail.com>
2021-07-23 12:07:46 +0200

    Import Home to App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import "./App.css";
 import Header from "./components/Header";
+import Home from "./components/Home";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <section className="home">
-        <h1>Welcome to the page</h1>
-        <img src="#" alt="Welcome to our page" />
-        <h2>Developed by me</h2>
-      </section>
+      <Home />
     </div>
   );
 }
